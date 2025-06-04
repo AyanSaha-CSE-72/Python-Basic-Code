cc7c91a7707456af2298f1f191820eb65a99aec2
Ayan Saha <143374035+AyanSaha-CSE-72@users.noreply.github.com>
2025-06-04 22:50:06 +0800

    Add files via upload

---
+++ check largest number.py
@@ -0,0 +1,11 @@
+
+a=int(input("First Number:"))
+
+b=int(input("Second Number:"))
+
+if a>b:
+    print("First Number is large")
+    print(a)
+else:
+    print("Second number is large ")
+    print(b)

9828719a19e122c841f71350bd99216f5ea69f93
Ayan Saha <143374035+AyanSaha-CSE-72@users.noreply.github.com>
2025-06-04 23:32:20 +0800

    Add files via upload

---
+++ marksheet.py
@@ -0,0 +1,25 @@
+mark=int(input("Enter The number:"))
+
+if mark>=80 and mark<=100:
+   print("Grade: A+")
+elif mark>=70 and mark<=79:
+   print("Grade: A")
+
+elif mark>=60 and mark<=69:
+   print("Grade: A-")
+
+
+elif mark >=50  and mark<=59:
+   print("Grade: B")
+
+elif mark >=40 and mark<=49:
+   print("Grade: C")
+
+elif mark >=33  and mark<=39:
+   print("Grade: D")
+
+elif mark >=00 and mark<=32:
+   print("Grade: Fail😢")
+
+else:
+ print("The number is invalide")

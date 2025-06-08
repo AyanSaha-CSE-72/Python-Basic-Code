3a631dfdd7a1e383f10cba6216805c9429f18bf2
Ayan Saha <143374035+AyanSaha-CSE-72@users.noreply.github.com>
2025-06-09 02:05:17 +0800

    Add files via upload

---
+++ Function with multiple parameters.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b): #- The def keyword is used to define a function.
+
+    return a + b
+
+result = add_numbers(5, 3) #- add_numbers() ফাংশনটি দুটি সংখ্যা গ্রহণ করে, সেগুলো যোগ করে এবং ফলাফল ফেরত দেয়।
+
+print("The sum is:", result) 

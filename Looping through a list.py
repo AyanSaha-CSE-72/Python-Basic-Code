9c0a4f50d91e66642058d8509fc597b3c420e0d6
Ayan Saha <143374035+AyanSaha-CSE-72@users.noreply.github.com>
2025-06-09 02:01:06 +0800

    Add files via upload

---
+++ Looping through a list.py
@@ -0,0 +1,6 @@
+fruits = ["Apple", "Banana", "Cherry","lychee"]
+
+for fruit in fruits:             #The for loop goes through each item in the list and prints "I like" followed by the fruit name.
+                                    #- for লুপ ব্যবহার করে লিস্টের প্রতিটি আইটেমের মাধ্যমে পুনরাবৃত্তি করে "I like" সহ প্রতিটি ফলের নাম প্রিন্ট করা হয়।
+
+    print("I like", fruit)

b729f658416dd06ee632aa97df71d8903fd4ad13
Ayan Saha <143374035+AyanSaha-CSE-72@users.noreply.github.com>
2025-06-10 22:51:38 +0800

    Add files via upload

---
+++ Creating and accessing a dictionary.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Ayan",
+    "age": 20,
+    "grade": "A+",
+    "is_enrolled": True
+}
+
+print(student["name"])     # Prints "Ayan"
+print(student["age"])     # Prints 20
+print(student["grade"])    # Prints "A+"
